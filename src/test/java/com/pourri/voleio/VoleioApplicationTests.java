e33bac9f18771a842f9b3a14e7c3b9c560b177c6
Rubinho01 <rubemkruger123@gmail.com>
2026-02-22 10:56:44 -0300

    Criação do Projeto + conexão com a database

---
+++ src/test/java/com/pourri/voleio/VoleioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pourri.voleio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
